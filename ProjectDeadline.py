6663e4e3dea9acd7a5edbc6d7784e35a3403fbb6
millsgerald <30513476+millsgerald@users.noreply.github.com>
2017-08-19 16:05:08 +1000

    Create ProjectDeadline.py

---
+++ ProjectDeadline.py
@@ -0,0 +1,3 @@
+#Ask a user to enter the deadline for their project
+# tell them how many days the have to complete the project
+# Give them the answer as a combination of weeks and days (use math funtion from module on numeric values)
